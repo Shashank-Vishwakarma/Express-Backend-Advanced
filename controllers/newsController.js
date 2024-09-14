d34c934d56f449808d296d525557cfc43b0251d1
Shashank <shashank78680@gmail.com>
2024-09-14 19:39:38 +0530

    news controller done

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,9 +1,10 @@
-import vine, { errors } from "@vinejs/vine";
+import vine, { errors, Vine } from "@vinejs/vine";
 import prisma from "../database/db.config.js";
 import { validateImage } from "../utils/validateImage.js";
 import { newsSchemaValidation } from "../schema/newsSchemavalidation.js";
 import { generateUniqueFileName } from "../utils/generateUniqueFileName.js";
 import { ENV_VARS } from "../utils/envVariables.js";
+import fs from 'fs'
 
 class NewsController {
     static async createNews(req, res) {
@@ -125,11 +126,18 @@ class NewsController {
     static async deletedNews(req, res) {
         try {
             const { id } = req.params;
-
             const news = await prisma.news.findUnique({ where: { id: parseInt(id) } });
             if (!news) {
                 return res.status(404).json({
                     error: "News not found"
+                });
+            }
+
+            // check if user is authorized to update
+            const isUserAuthorized = req.user.id === news.userId;
+            if (!isUserAuthorized) {
+                return res.status(401).json({
+                    error: "Not Authorized"
                 });
             }
 
@@ -149,7 +157,61 @@ class NewsController {
 
     static async updateNews(req, res) {
         try {
+            const { id } = req.params;
+            const news = await prisma.news.findUnique({ where: { id: parseInt(id) } });
+            if (!news) {
+                return res.status(404).json({
+                    error: "News not found"
+                });
+            }
 
+            // check if user is authorized to update
+            const isUserAuthorized = req.user.id === news.userId;
+            if (!isUserAuthorized) {
+                return res.status(401).json({
+                    error: "Not Authorized"
+                });
+            }
+
+            const { title, body } = req.body;
+
+            const image = req.files?.image;
+            let fileName = "";
+            if (image) { // delete old image
+                const oldImagePath = process.cwd() + "/public/news/" + news.image.split('/').pop();
+                if (!fs.existsSync(oldImagePath)) {
+                    return res.status(404).json({
+                        error: "Image not found"
+                    });
+                }
+                fs.rmSync(oldImagePath);
+
+                const { error } = validateImage(image.mimetype);
+                if (error !== null) return res.status(400).json({ error });
+
+                fileName = generateUniqueFileName(image.name);
+                const uploadPath = process.cwd() + "/public/news/" + fileName;
+                image.mv(uploadPath);
+            }
+
+            await prisma.news.update({
+                where: { id: parseInt(id) },
+                data: {
+                    title: title || news.title,
+                    body: body || news.body,
+                    image: image ? ENV_VARS.APP_URL + "/news/" + fileName : news.image
+                }
+            })
+
+            return res.status(200).json({
+                success: true,
+                message: "News updated successfully",
+                news: {
+                    title: title || news.title,
+                    body: body || news.body,
+                    image: image ? ENV_VARS.APP_URL + "/news/" + fileName : news.image
+                }
+            });
         } catch (error) {
             console.log("Error in updateNews Api: ", error);
             return res.status(500).json({
